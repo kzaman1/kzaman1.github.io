8daf7f00ef74b7dc0b18a6b52ea3db070f5705b4
Khuram Zaman <khuram.zaman1@gmail.com>
2020-09-12 18:41:52 -0400

    (1) Added music and (2) added more upgrades.
    I added some music (although I can't seem to get it to auto-loop). I also added a few more upgrades: Juice Bot, Juice Drone, and Juice Factory. Also, in the css, instead of replicating each individual button, I styled all of the buttons to look the same and also standardized the hover.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,22 @@ var upJuicePressRate = 1;
 //Juice Machine Upgrade Variables
 var upJuiceMachineCount = 0;
 var upJuiceMachineCost = 20;
-var upJuiceMachineRate = 1000;
+var upJuiceMachineRate = 10;
+
+//Juice Bot Upgrade Variables
+var upJuiceBotCount = 0;
+var upJuiceBotCost = 100;
+var upJuiceBotRate = 100;
+
+//Juice Drone Upgrade Variables
+var upJuiceDroneCount = 0;
+var upJuiceDroneCost = 1000;
+var upJuiceDroneRate = 1000;
+
+//Juice Factory Upgrade Variables
+var upJuiceFactoryCount = 0;
+var upJuiceFactoryCost = 1000;
+var upJuiceFactoryRate = 1000;
 
 // This function updates the mango juices made so you don't have to copy / paste the same code over and over again //
 function updateMangoJuiceDisplay(){
@@ -20,6 +35,8 @@ function make(){
   mangojuices++; // Same as 'mangojuices = mangojuices + 1' or 'mangojuices +=1'
   updateMangoJuiceDisplay();
 }
+
+// Upgrade Functions
 
 function buyJuicePress() {
   if(mangojuices >= upJuicePressCost) {
@@ -41,10 +58,41 @@ function buyJuiceMachine() {
   }
 }
 
-window.setInterval(mainLoop, 1000);
+function buyJuiceBot() {
+  if(mangojuices >= upJuiceBotCost) {
+    upJuiceBotCount++;
+    mangojuices = mangojuices - upJuiceBotCost;
+
+    document.getElementById('JuiceBotCount').innerHTML = upJuiceBotCount;
+    updateMangoJuiceDisplay()
+  }
+}
+
+function buyJuiceDrone() {
+  if(mangojuices >= upJuiceDroneCost) {
+    upJuiceDroneCount++;
+    mangojuices = mangojuices - upJuiceDroneCost;
+
+    document.getElementById('JuiceDroneCount').innerHTML = upJuiceDroneCount;
+    updateMangoJuiceDisplay()
+  }
+}
+
+function buyJuiceFactory() {
+  if(mangojuices >= upJuiceFactoryCost) {
+    upJuiceFactoryCount++;
+    mangojuices = mangojuices - upJuiceFactoryCost;
+
+    document.getElementById('JuiceFactoryCount').innerHTML = upJuiceFactoryCount;
+    updateMangoJuiceDisplay()
+  }
+}
+
 
 // This is the main loop. This is where a lot of the action happens //
+window.setInterval(mainLoop, 1000);
+
 function mainLoop(){
-  mangojuices = mangojuices + (upJuicePressCount * upJuicePressRate) + (upJuiceMachineCount * upJuiceMachineRate);
+  mangojuices = mangojuices + (upJuicePressCount * upJuicePressRate) + (upJuiceMachineCount * upJuiceMachineRate) + (upJuiceBotCount * upJuiceBotRate) + (upJuiceDroneCount * upJuiceDroneRate) + (upJuiceFactoryCount * upJuiceFactoryCount);
   updateMangoJuiceDisplay()
 }
